87cae253038154382dedde1ba6f67cb79b4d264f
Šimon Leitgeb <leitgeb.simon@gmail.com>
2015-11-04 21:03:31 +0100

    Finished covertor and first try on the GMaps iframe.

--- convert_coordinates.js
+++ convert_coordinates.js
@@ -11,18 +11,13 @@ function parseCoords(axis) {
 	var M = getElement(axis + "CoordM").value;
 	var S = getElement(axis + "CoordS").value;
 	var Mod = getElement(axis + "Mod").value;
-	var DMS = String(Math.round(convertDMS(D, M, S)*100000)/100000);
-	var dir;
-	if (axis == "Lat") {
-		dir = Mod -1 ? " j.š." : " s.š.";
-	} else {
-		dir = Mod -1 ? " z.d." : " v.d.";
-	}
-	return DMS + dir;
+	return Mod*Math.round(convertDMS(D, M, S)*100000)/100000;
 }
 
 function displayCoords() {
-	document.getElementById("result").innerHTML = parseCoords("Lat") + "<br>" + parseCoords("Lon");
+	var LatMod = getElement("LatMod").value == -1 ? " j.š." : " s.š.";
+	var LonMod = getElement("LonMod").value == -1 ? " z.d." : " v.d.";
+	document.getElementById("result").innerHTML = String(Math.abs(parseCoords("Lat"))) + LatMod + "<br>" + String(Math.abs(parseCoords("Lon"))) + LonMod;
 }
 
 function getMax(field) {
@@ -31,7 +26,16 @@ function getMax(field) {
 	} else if (field == "LonCoordD" || field == "LonRow") {
 		return 180;
 	} else if (['LatCoordM', 'LatCoordS', 'LonCoordM', 'LonCoordS'].indexOf(field) > -1) {
-		return 60;
+		return 59;
+	}
+}
+
+function markError(field, bool) {
+	field.className = field.className;
+	if (bool) {
+		field.className = field.className.indexOf('error') == -1 ? field.className += " error" : field.className;
+	} else {
+		field.className = field.className.indexOf('error') != -1 ? field.className.replace(" error", "") : field.className;
 	}
 }
 
@@ -40,25 +44,46 @@ function validateValue(value, max) {
 }
 
 function validateField(field) {
-	var maxValue = getMax(field);
-	var element = getElement(field);
-	var validValue = validateValue(element.value, maxValue);
-	var D = getElement(field.substr(0, 8) + "D");
-	var M = getElement(field.substr(0, 8) + "M");
-	var S = getElement(field.substr(0, 8) + "S");
-	var validValues = validateValue(D.value, getMax(field.substr(0, 8) + "D")) && validateValue(M.value, 60) && validateValue(S.value, 60);
-	var validRow = validValues && validateValue(convertDMS(D.value, M.value, S.value), getMax(field.substr(0, 3) + "Row"))
-	if (!validValue && element.className.indexOf('error') == -1) {
-		element.className += " error";
-	} else if (validValue && element.className.indexOf('error') != -1) {
-		element.className = element.className.replace(" error", "");
+	var M = getElement(field.substr(0,3) + "CoordM");
+	var S = getElement(field.substr(0,3) + "CoordS");
+	var rowValidity = validateRow(field.substr(0,3));
+
+	if (!rowValidity) {
+		markError(M, M.value != 0);
+		markError(S, S.value != 0);
+	} else if (rowValidity) {
+		markError(M, !validateValue(M.value, 59));
+		markError(S, !validateValue(S.value, 59));
 	}
-	if (validValues && !validRow) {
-		M.className = (M.value != 0 && M.className.indexOf('error') == -1) ? M.className + " error" : M.className;
-		S.className = (S.value != 0 && S.className.indexOf('error') == -1) ? S.className + " error" : S.className;
-	} else if (validRow) {
-		D.className = (D.className.indexOf('error') != -1) ? D.className.replace(" error", "") : D.className;
-		M.className = (M.className.indexOf('error') != -1) ? M.className.replace(" error", "") : M.className;
-		S.className = (S.className.indexOf('error') != -1) ? S.className.replace(" error", "") : S.className;
+	
+	markError(getElement(field), !/^[0-9]{1,3}$/.test(getElement(field).value) || !validateValue(getElement(field).value, getMax(field)));
+}
+
+function validateRow(row) {
+	var D = getElement(row + "CoordD");
+	var M = getElement(row + "CoordM");
+	var S = getElement(row + "CoordS");
+	return (D.value == getMax(row + "CoordD") && M.value == 0 && S.value == 0) || D.value < getMax(row + "CoordD")
+}
+
+function validateForm() {
+	var errors = "";
+	var LatD = getElement("LatCoordD").value;
+	var LatM = getElement("LatCoordM").value;
+	var LatS = getElement("LatCoordS").value;
+	var LonD = getElement("LonCoordD").value;
+	var LonM = getElement("LonCoordM").value;
+	var LonS = getElement("LonCoordS").value;
+
+	errors += (!validateRow("Lat")) ? "Zeměpisná šířka nesmí být větší než 90°.<br>" : "";
+	errors += (!validateRow("Lon")) ? "Zeměpisná délka nesmí být větší než 180°.<br>" : "";
+	errors += (!validateValue(LatM, 59) || !validateValue(LonM, 59)) ? "Minuty musí být menší než 60'.<br>" : "";
+	errors += (!validateValue(LatS, 59) || !validateValue(LonS, 59)) ? "Sekundy musí být menší než 60\".<br>" : "";
+	errors += (!/^[0-9]{1,3}$/.test(LatD)) ? "Souřadnice mohou obsahovat pouze celá čísla." : "";
+
+	if (errors == "") {
+		displayCoords();
+	} else {
+		document.getElementById("result").innerHTML = errors;
 	}
 }
