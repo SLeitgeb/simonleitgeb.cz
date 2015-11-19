a0458c27f7c6a5f14a909af31060ea40023e5abf
Šimon Leitgeb <leitgeb.simon@gmail.com>
2015-11-20 00:07:19 +0100

    Improvement in the leaflet map of public transport. Completed visualisation of points with route labels, colors based on vehicle type and rotation with the correct bearing.

--- map.js
+++ map.js
@@ -1,3 +1,43 @@
+function onEachFeature(feature, layer) {
+	var popupContent = "<h3>" + feature.properties.routeLabel + " &rarr; " + feature.properties.headsign + "</h3>";
+	layer.bindPopup(popupContent);
+}
+
+var trafficIcon = L.icon({
+	iconUrl: 'icon.svg',
+	iconSize: [30, 30],
+	iconAnchor: [21, 15],
+	popupAnchor: [0, -15]
+});
+
+var busIcon = L.icon({
+	iconUrl: 'icon_bus.svg',
+	iconSize: [30, 30],
+	iconAnchor: [21, 15],
+	popupAnchor: [0, -15]
+});
+
+var nbusIcon = L.icon({
+	iconUrl: 'icon_nbus.svg',
+	iconSize: [30, 30],
+	iconAnchor: [21, 15],
+	popupAnchor: [0, -15]
+});
+
+var tramIcon = L.icon({
+	iconUrl: 'icon_tram.svg',
+	iconSize: [30, 30],
+	iconAnchor: [21, 15],
+	popupAnchor: [0, -15]
+});
+
+var trolIcon = L.icon({
+	iconUrl: 'icon_trol.svg',
+	iconSize: [30, 30],
+	iconAnchor: [21, 15],
+	popupAnchor: [0, -15]
+});
+
 var OSMlayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 });
@@ -15,6 +55,47 @@ var KladZM10 = L.tileLayer.wms("http://geoportal.cuzk.cz/WMS_KLADY/WMService.asp
 	transparent: true
 });
 
+var traffic = L.geoJson([], {
+	onEachFeature: onEachFeature,
+
+	pointToLayer: function (feature, latlng) {
+		console.log(feature.properties.bearing);
+		var val = feature.properties.routeLabel;
+		var rotate = feature.properties.bearing + 180;
+		if (val < 20) {return L.rotatedMarker(latlng, {icon: tramIcon, angle: rotate });} else
+		if (val < 40) {return L.rotatedMarker(latlng, {icon: trolIcon, angle: rotate });} else
+		if (val < 90) {return L.rotatedMarker(latlng, {icon: busIcon, angle: rotate });} else
+		if (val < 100) {return L.rotatedMarker(latlng, {icon: nbusIcon, angle: rotate });} else
+		{return L.rotatedMarker(latlng, {icon: trafficIcon, angle: rotate });}
+	}
+});
+
+var trafficNumbers = L.geoJson([], {
+	pointToLayer: function (feature, latlng) {
+		var trafficNumber = L.divIcon({
+			html: "<h5>" + feature.properties.routeLabel + "</h5>",
+			iconSize: [18,18],
+			iconAnchor: [9,9]
+		});
+
+		return L.marker(latlng, {
+			icon: trafficNumber,
+			clickable: false
+		});
+	}
+});
+
+$.ajax({
+dataType: "json",
+url: "poloha.geojson",
+success: function(data) {
+    $(data.features).each(function(key, data) {
+        trafficNumbers.addData(data);
+        traffic.addData(data);
+    });
+}
+}).error(function() {});
+
 // var trasicka = L.polyline([
 // 	[49.2049642, 16.5972086],
 // 	[49.2055775, 16.5970050],
@@ -29,8 +110,8 @@ var KladZM10 = L.tileLayer.wms("http://geoportal.cuzk.cz/WMS_KLADY/WMService.asp
 
 var map = L.map ("map", {
 	center: [49.2, 16.6],
-	zoom: 13,
-	layers: [OSMlayer]
+	zoom: 16,
+	layers: [OSMlayer, trafficNumbers, traffic]
 });
 
 var baseLayers = {
@@ -40,22 +121,25 @@ var baseLayers = {
 var overlays = {
 	"Hillshade": WMSHillshade,
 	"Klad ZM10": KladZM10,
+	"Poloha MHD": traffic
 	// "Trasa": trasicka
 }
 
 L.control.layers(baseLayers, overlays).addTo(map);
 
-map.locate({
-	watch: true,
-	setView: true,
-	maxZoom: 15
-});
-
-function displayTraffic() {
-	$.getJSON("http://sotoris.cz/DataSource/CityHack2015/vehiclesBrno.aspx", function(data) {
-		console.log(data);
-	});
+function locate() {
+	map.locate({
+		watch: true,
+		setView: true,
+		maxZoom: 15
+	});	
 }
+
+// L.circleMarker([49.20301, 16.64184], {
+// 	radius: 3
+// }).addTo(map);
+
+// map.panTo([49.20301, 16.64184]);
 
 // $.ajax({
 //     url: 'http://sotoris.cz/DataSource/CityHack2015/vehiclesBrno.aspx',
